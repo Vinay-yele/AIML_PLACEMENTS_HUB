11530a99b6a209772db64e04f47db911a2481419
Vinay Yele <vinayyele1998@gmail.com>
2025-07-18 15:23:37 +0530

    Fix: CORS policy and correct API endpoints for deployed app

--- backend/routes/announcementRoutes.js
+++ backend/routes/announcementRoutes.js
@@ -1,8 +1,12 @@
+
+
 const express = require('express');
 const router = express.Router();
 const announcementController = require('../controllers/announcementController');
 
-router.get('/getan', announcementController.getAllAnnouncements);
-router.post('/creatan', announcementController.createAnnouncement);
+
+router.get('/', announcementController.getAllAnnouncements);
+
+router.post('/', announcementController.createAnnouncement);
 
 module.exports = router;
