2197c47062af0893313aa25d4a7b9e60d24f86c4
Vinay Yele <vinayyele1998@gmail.com>
2025-07-28 19:06:20 +0530

    worked on annouicnement delete route

--- backend/routes/announcementRoutes.js
+++ backend/routes/announcementRoutes.js
@@ -9,4 +9,5 @@ router.get('/', announcementController.getAllAnnouncements);
 
 router.post('/', announcementController.createAnnouncement);
 
+router.delete('/:id', announcementController.deleteAnnouncement);
 module.exports = router;
