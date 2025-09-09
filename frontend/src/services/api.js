5f7bfa0a9a0de7073db6a23c7bc6ddd38eaa5607
Vinay Yele <vinayyele1998@gmail.com>
2025-09-10 05:25:24 +0530

    rebase32

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-        baseURL: 'https://aiml-placements-hub.onrender.com'
+      baseURL: 'https://aiml-placements-hub.onrender.com/api'
     });
 
 // --- Announcements ---
